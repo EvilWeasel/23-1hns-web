bf7e97bf42bf7e94270273093d39202bf6d87c13
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2024-02-13 11:03:36 +0100

    Started working with JavaScript; Added Variable Deklarations; Added Primitive Types; Added function and caller;

--- hello_node.js
+++ hello_node.js
@@ -29,5 +29,35 @@ greeting("Hier könnte Ihre Werbung stehen!");
 // Dieser Prozess nennt sich "Hoisting"
 function greeting(message) {
     console.log("Hallo und Wilkommen!");
-    console.log(`Schönes Wetter haben wir, übrigens... \n${message}`);
+    console.log(`Schönes Wetter haben wir, {} übrigens... \n${message}`);
+    // Console.WriteLine($"Bla {Test} {Test2}");
 }
+
+// Primitve Values => Wertetypen
+// Alle Primitven Typen haben eine Klasse (eg. String oder Number) mit Helfermethoden
+const my_string = "10"; // Console.ReadLine()
+const user_input = Number.parseInt(my_string);
+// Boolean
+// 0 oder 1; true oder false
+const my_bools = false;
+if (my_bools) {
+    console.log("Dieser Text wird niemals ausgegeben.");
+}
+
+// Numbers
+// Ints, Floats, Doubles, Decimals, Longs gibt es Nicht!
+// In JS gibt es nur einen Datentyp für Zahlen.
+let my_number1 = 10;
+let my_number2 = 5.2;
+
+// Achtung: Vorsicht!
+// Vorallem bei Division muss man in JS Error-Checking einbauen
+if (my_number2 != null || my_number2 != 0) {
+    const result = my_number1 / my_number2;
+}
+
+const num1 = 0.1;
+const num2 = 0.2;
+console.log(num1 + num2); // Floating-Point Rounding Error
+// Verhält sich genau wie im Dezimalsystem:
+    // 0.33333 + 0.33333 + 0.33333 = 1
