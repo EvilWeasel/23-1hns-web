8c4c9ebe9079d00625c41ab7e1ff55db122baf54
Tobias Wehrle <38261180+EvilWeasel@users.noreply.github.com>
2024-02-18 15:54:18 +0100

    added starlight

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), starlight()]
 });
